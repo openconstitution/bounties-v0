9fcd8b6ebad4aad367a42bf56e653c619046a59b
xurror <kaze.nasser@outlook.com>
2020-09-29 05:01:30 +0100

    swap user-profile relation owner

--- src/main/java/org/muellners/bounties/domain/User.java
+++ src/main/java/org/muellners/bounties/domain/User.java
@@ -25,124 +25,126 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @org.springframework.data.elasticsearch.annotations.Document(indexName = "user")
 public class User extends AbstractAuditingEntity {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  @Id
-  @org.springframework.data.elasticsearch.annotations.
-  Field(type = FieldType.Keyword)
-  private String id;
+    @Id
+    @org.springframework.data.elasticsearch.annotations.
+    Field(type = FieldType.Keyword)
+    private String id;
 
-  @NotNull
-  @Pattern(regexp = Constants.LOGIN_REGEX)
-  @Size(min = 1, max = 50)
-  @Column(length = 50, unique = true, nullable = false)
-  private String login;
+    @NotNull
+    @Pattern(regexp = Constants.LOGIN_REGEX)
+    @Size(min = 1, max = 50)
+    @Column(length = 50, unique = true, nullable = false)
+    private String login;
 
-  @Size(max = 50)
-  @Column(name = "first_name", length = 50)
-  private String firstName;
+    @Size(max = 50)
+    @Column(name = "first_name", length = 50)
+    private String firstName;
 
-  @Size(max = 50)
-  @Column(name = "last_name", length = 50)
-  private String lastName;
+    @Size(max = 50)
+    @Column(name = "last_name", length = 50)
+    private String lastName;
 
-  @Email
-  @Size(min = 5, max = 254)
-  @Column(length = 254, unique = true)
-  private String email;
+    @Email
+    @Size(min = 5, max = 254)
+    @Column(length = 254, unique = true)
+    private String email;
 
-  @NotNull @Column(nullable = false) private boolean activated = false;
+	@NotNull @Column(nullable = false)
+	private boolean activated = false;
 
-  @Size(min = 2, max = 10)
-  @Column(name = "lang_key", length = 10)
-  private String langKey;
+    @Size(min = 2, max = 10)
+    @Column(name = "lang_key", length = 10)
+    private String langKey;
 
-  @Size(max = 256)
-  @Column(name = "image_url", length = 256)
-  private String imageUrl;
+    @Size(max = 256)
+    @Column(name = "image_url", length = 256)
+    private String imageUrl;
 
-  @OneToOne @JoinColumn(name = "profile_id") private Profile profile;
+	@OneToOne @JoinColumn(name = "profile_id")
+	private Profile profile;
 
-  @JsonIgnore
-  @ManyToMany
-  @JoinTable(
-      name = "jhi_user_authority",
-      joinColumns =
-      { @JoinColumn(name = "user_id", referencedColumnName = "id") },
-      inverseJoinColumns =
-      { @JoinColumn(name = "authority_name", referencedColumnName = "name") })
-  @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-  @BatchSize(size = 20)
-  private Set<Authority> authorities = new HashSet<>();
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+        name = "jhi_user_authority",
+        joinColumns =
+        { @JoinColumn(name = "user_id", referencedColumnName = "id") },
+        inverseJoinColumns =
+        { @JoinColumn(name = "authority_name", referencedColumnName = "name") })
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    @BatchSize(size = 20)
+    private Set<Authority> authorities = new HashSet<>();
 
-  public Profile getProfile() { return profile; }
+    public Profile getProfile() { return profile; }
 
-  public void setProfile(Profile profile) { this.profile = profile; }
+    public void setProfile(Profile profile) { this.profile = profile; }
 
-  public String getId() { return id; }
+    public String getId() { return id; }
 
-  public void setId(String id) { this.id = id; }
+    public void setId(String id) { this.id = id; }
 
-  public String getLogin() { return login; }
+    public String getLogin() { return login; }
 
-  // Lowercase the login before saving it in database
-  public void setLogin(String login) {
-    this.login = StringUtils.lowerCase(login, Locale.ENGLISH);
-  }
-
-  public String getFirstName() { return firstName; }
-
-  public void setFirstName(String firstName) { this.firstName = firstName; }
-
-  public String getLastName() { return lastName; }
-
-  public void setLastName(String lastName) { this.lastName = lastName; }
-
-  public String getEmail() { return email; }
-
-  public void setEmail(String email) { this.email = email; }
-
-  public String getImageUrl() { return imageUrl; }
-
-  public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
-
-  public boolean getActivated() { return activated; }
-
-  public void setActivated(boolean activated) { this.activated = activated; }
-
-  public String getLangKey() { return langKey; }
-
-  public void setLangKey(String langKey) { this.langKey = langKey; }
-
-  public Set<Authority> getAuthorities() { return authorities; }
-
-  public void setAuthorities(Set<Authority> authorities) {
-    this.authorities = authorities;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    // Lowercase the login before saving it in database
+    public void setLogin(String login) {
+      	this.login = StringUtils.lowerCase(login, Locale.ENGLISH);
     }
-    if (!(o instanceof User)) {
-      return false;
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getImageUrl() { return imageUrl; }
+
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+
+    public boolean getActivated() { return activated; }
+
+    public void setActivated(boolean activated) { this.activated = activated; }
+
+    public String getLangKey() { return langKey; }
+
+    public void setLangKey(String langKey) { this.langKey = langKey; }
+
+    public Set<Authority> getAuthorities() { return authorities; }
+
+    public void setAuthorities(Set<Authority> authorities) {
+      	this.authorities = authorities;
     }
-    return id != null && id.equals(((User)o).id);
-  }
 
-  @Override
-  public int hashCode() {
-    return 31;
-  }
+    @Override
+    public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof User)) {
+			return false;
+		}
+		return id != null && id.equals(((User)o).id);
+    }
 
-  // prettier-ignore
-  @Override
-  public String toString() {
-    return "User{"
-        + "login='" + login + '\'' + ", firstName='" + firstName + '\'' +
-        ", lastName='" + lastName + '\'' + ", email='" + email + '\'' +
-        ", imageUrl='" + imageUrl + '\'' + ", activated='" + activated + '\'' +
-        ", langKey='" + langKey + '\'' + "}";
-  }
+    @Override
+    public int hashCode() {
+      return 31;
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+      return "User{"
+          + "login='" + login + '\'' + ", firstName='" + firstName + '\'' +
+          ", lastName='" + lastName + '\'' + ", email='" + email + '\'' +
+          ", imageUrl='" + imageUrl + '\'' + ", activated='" + activated + '\'' +
+          ", langKey='" + langKey + '\'' + "}";
+    }
 }
