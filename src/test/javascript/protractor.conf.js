43f1f63fb103c1f7b523b83209c512e758131857
Restyled.io <commits@restyled.io>
2020-09-30 12:20:37 +0000

    Restyled by prettier

--- src/test/javascript/protractor.conf.js
+++ src/test/javascript/protractor.conf.js
@@ -1,35 +1,35 @@
-
 exports.config = {
-  allScriptsTimeout : 60000,
+  allScriptsTimeout: 60000,
 
-  specs : [
-    './e2e/modules/account/*.spec.ts', './e2e/modules/administration/*.spec.ts',
-    './e2e/entities/**/*.spec.ts'
+  specs: [
+    './e2e/modules/account/*.spec.ts',
+    './e2e/modules/administration/*.spec.ts',
+    './e2e/entities/**/*.spec.ts',
     /* jhipster-needle-add-protractor-tests - JHipster will add protractors
        tests here */
   ],
 
-  capabilities : {
-    browserName : 'chrome',
-    chromeOptions : {
-      args : process.env.JHI_E2E_HEADLESS
-                 ? [ '--headless', '--disable-gpu', '--window-size=800,600' ]
-                 : [ '--disable-gpu', '--window-size=800,600' ]
-    }
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: process.env.JHI_E2E_HEADLESS
+        ? ['--headless', '--disable-gpu', '--window-size=800,600']
+        : ['--disable-gpu', '--window-size=800,600'],
+    },
   },
 
-  directConnect : true,
+  directConnect: true,
 
-  baseUrl : 'http://localhost:8080/',
+  baseUrl: 'http://localhost:8080/',
 
-  framework : 'mocha',
+  framework: 'mocha',
 
-  SELENIUM_PROMISE_MANAGER : false,
+  SELENIUM_PROMISE_MANAGER: false,
 
-  mochaOpts : {reporter : 'spec', slow : 6000, ui : 'bdd', timeout : 60000},
+  mochaOpts: { reporter: 'spec', slow: 6000, ui: 'bdd', timeout: 60000 },
 
   beforeLaunch() {
-    require('ts-node').register({project : './tsconfig.e2e.json'});
+    require('ts-node').register({ project: './tsconfig.e2e.json' });
   },
 
   onPrepare() {
@@ -46,5 +46,5 @@ exports.config = {
     // @ts-ignore
     global.chai = chai;
   },
-  params : {waitTimeoutInMillis : 10000, logWaitErrors : false}
+  params: { waitTimeoutInMillis: 10000, logWaitErrors: false },
 };
--- webpack/utils.js
+++ webpack/utils.js
@@ -4,36 +4,36 @@ const tsconfig = require('../tsconfig.json');
 
 module.exports = {
   root,
-  mapTypescriptAliasToWebpackAlias
+  mapTypescriptAliasToWebpackAlias,
 };
 
 const _root = path.resolve(__dirname, '..');
 
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [ _root ].concat(args));
+  return path.join.apply(path, [_root].concat(args));
 }
 
 function mapTypescriptAliasToWebpackAlias(alias = {}) {
-  const webpackAliases = {...alias};
+  const webpackAliases = { ...alias };
   if (!tsconfig.compilerOptions.paths) {
     return webpackAliases;
   }
   Object.entries(tsconfig.compilerOptions.paths)
-      .filter(([ key, value ]) => {
-        // use Typescript alias in Webpack only if this has value
-        return !!value.length;
-      })
-      .map(([ key, value ]) => {
-        // if Typescript alias ends with /* then remove this for Webpack
-        const regexToReplace = /\/\*$/;
-        const aliasKey = key.replace(regexToReplace, '');
-        const aliasValue = value[0].replace(regexToReplace, '');
-        return [ aliasKey, root(aliasValue) ];
-      })
-      .reduce((aliases, [ key, value ]) => {
-        aliases[key] = value;
-        return aliases;
-      }, webpackAliases);
+    .filter(([key, value]) => {
+      // use Typescript alias in Webpack only if this has value
+      return !!value.length;
+    })
+    .map(([key, value]) => {
+      // if Typescript alias ends with /* then remove this for Webpack
+      const regexToReplace = /\/\*$/;
+      const aliasKey = key.replace(regexToReplace, '');
+      const aliasValue = value[0].replace(regexToReplace, '');
+      return [aliasKey, root(aliasValue)];
+    })
+    .reduce((aliases, [key, value]) => {
+      aliases[key] = value;
+      return aliases;
+    }, webpackAliases);
   return webpackAliases;
 }
