12059ef3af5168329d9ea7e42717ec7a1046b435
xurror <kaze.nasser@outlook.com>
2020-10-05 17:28:20 +0100

    interfacing expiry date countdown timer

--- src/main/webapp/app/shared/util/date-utils.ts
+++ src/main/webapp/app/shared/util/date-utils.ts
@@ -7,3 +7,30 @@ export const convertDateTimeFromServer = date => (date ? moment(date).format(APP
 export const convertDateTimeToServer = date => (date ? moment(date, APP_LOCAL_DATETIME_FORMAT_Z).toDate() : null);
 
 export const displayDefaultDateTime = () => moment().startOf('day').format(APP_LOCAL_DATETIME_FORMAT);
+
+export const calculateTimeLeft = (endDate: string) => {
+	const date = new Date(endDate).getTime()
+	const difference = +new Date(endDate) - +new Date();
+	let timeLeft = {};
+
+	const then = moment(endDate)
+	const now = moment()
+	
+	const countdown = then.diff(now)
+	
+	const days = moment(countdown).format('DDDD');
+	const hours = moment(countdown).format('HH');
+	const minutes = moment(countdown).format('mm');
+	const seconds = moment(countdown).format('ss');
+
+	if (countdown >= 0) {
+		timeLeft = {
+			days,
+			hours,
+			minutes,
+			seconds
+		}
+	}
+
+	return timeLeft;
+};
