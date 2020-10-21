cba628656ec5c4d1a0450a474d3c1734e9cac897
xurror <kaze.nasser@outlook.com>
2020-10-21 18:10:44 +0100

    create profile main page

---
+++ src/main/webapp/app/entities/profile/Card/cardIconStyle.ts
@@ -0,0 +1,44 @@
+import {
+  warningCardHeader,
+  successCardHeader,
+  dangerCardHeader,
+  infoCardHeader,
+  primaryCardHeader,
+  roseCardHeader
+} from "../style";
+
+const grayColor = [
+  "#999",
+  "#777",
+  "#3C4858",
+  "#AAAAAA",
+  "#D2D2D2",
+  "#DDD",
+  "#b4b4b4",
+  "#555555",
+  "#333",
+  "#a9afbb",
+  "#eee",
+  "#e7e7e7"
+];
+
+const cardIconStyle = {
+  cardIcon: {
+    "&$warningCardHeader,&$successCardHeader,&$dangerCardHeader,&$infoCardHeader,&$primaryCardHeader,&$roseCardHeader": {
+      borderRadius: "3px",
+      backgroundColor: grayColor[0],
+      padding: "15px",
+      marginTop: "-20px",
+      marginRight: "15px",
+      float: "left"
+    }
+  },
+  warningCardHeader,
+  successCardHeader,
+  dangerCardHeader,
+  infoCardHeader,
+  primaryCardHeader,
+  roseCardHeader
+};
+
+export default cardIconStyle;
--- src/main/webapp/app/entities/profile/index.tsx
+++ src/main/webapp/app/entities/profile/index.tsx
@@ -7,16 +7,18 @@ import Profile from './profile';
 import ProfileDetail from './profile-detail';
 import ProfileUpdate from './profile-update';
 import ProfileDeleteDialog from './profile-delete-dialog';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { AUTHORITIES } from 'app/config/constants';
 
 const Routes = ({ match }) => (
   <>
     <Switch>
-      <ErrorBoundaryRoute exact path={`${match.url}/new`} component={ProfileUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={ProfileUpdate} />
+      <PrivateRoute exact path={`${match.url}/new`} component={ProfileUpdate} hasAnyAuthorities={[AUTHORITIES.USER]} />
+      <PrivateRoute exact path={`${match.url}/:id/edit`} component={ProfileUpdate} hasAnyAuthorities={[AUTHORITIES.USER]} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={ProfileDetail} />
       <ErrorBoundaryRoute path={match.url} component={Profile} />
     </Switch>
-    <ErrorBoundaryRoute exact path={`${match.url}/:id/delete`} component={ProfileDeleteDialog} />
+    <PrivateRoute exact path={`${match.url}/:id/delete`} component={ProfileDeleteDialog} hasAnyAuthorities={[AUTHORITIES.USER]} />
   </>
 );
 
