87ff38da18d9df51fb36ed661e26ee328882b5b9
xurror <kaze.nasser@outlook.com>
2020-10-23 19:27:43 +0100

    refine secured and unsecured UI resources'

--- src/main/webapp/app/entities/funding/index.tsx
+++ src/main/webapp/app/entities/funding/index.tsx
@@ -7,16 +7,18 @@ import Funding from './funding';
 import FundingDetail from './funding-detail';
 import FundingUpdate from './funding-update';
 import FundingDeleteDialog from './funding-delete-dialog';
+import PrivateRoute from 'app/shared/auth/private-route';
+import { AUTHORITIES } from 'app/config/constants';
 
 const Routes = ({ match }) => (
   <>
     <Switch>
-      <ErrorBoundaryRoute exact path={`${match.url}/new`} component={FundingUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={FundingUpdate} />
+      <PrivateRoute exact path={`${match.url}/new`} component={FundingUpdate} hasAnyAuthorities={[AUTHORITIES.USER]} />
+      <PrivateRoute exact path={`${match.url}/:id/edit`} component={FundingUpdate} hasAnyAuthorities={[AUTHORITIES.USER]} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={FundingDetail} />
       <ErrorBoundaryRoute path={match.url} component={Funding} />
     </Switch>
-    <ErrorBoundaryRoute exact path={`${match.url}/:id/delete`} component={FundingDeleteDialog} />
+    <PrivateRoute exact path={`${match.url}/:id/delete`} component={FundingDeleteDialog} hasAnyAuthorities={[AUTHORITIES.USER]} />
   </>
 );
 
