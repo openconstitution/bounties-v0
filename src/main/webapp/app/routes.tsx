2034dc8b17ce59b8e537ce2fe1e2b0d54093dc65
xurror <kaze.nasser@outlook.com>
2020-11-03 20:46:52 +0100

    account module in routes

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -6,7 +6,9 @@ import Logout from 'app/modules/login/logout';
 import Login from 'app/modules/login/login';
 import Home from 'app/modules/home/home';
 
-import Entities from 'app/entities';
+import Profile from 'app/modules/account/profile/profile';
+import Settings from 'app/modules/account/settings/settings';
+
 import PrivateRoute from 'app/shared/auth/private-route';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import PageNotFound from 'app/shared/error/page-not-found';
@@ -17,6 +19,11 @@ const Admin = Loadable({
   loading: () => <div>loading ...</div>,
 });
 
+const Account = Loadable({
+  loader: () => import(/* webpackChunkName: "administration" */ 'app/modules/account'),
+  loading: () => <div>loading ...</div>,
+});
+
 const Routes = () => (
   <div className="view-routes">
     <Switch>
@@ -24,8 +31,10 @@ const Routes = () => (
       <ErrorBoundaryRoute path="/login" exact component={Login} />
       <ErrorBoundaryRoute path="/" exact component={Home} />
 
-      <PrivateRoute path="/admin" component={Admin} hasAnyAuthorities={[AUTHORITIES.ADMIN]} />      
-      <ErrorBoundaryRoute path="/" component={Entities} />
+      <ErrorBoundaryRoute path="/" component={Account} />
+      <PrivateRoute path="/admin" component={Admin} hasAnyAuthorities={[AUTHORITIES.ADMIN]} />
+
+      {/* <ErrorBoundaryRoute path="/" component={Entities} /> */}
       <ErrorBoundaryRoute component={PageNotFound} />
     </Switch>
   </div>
