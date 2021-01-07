14bf111cb7f4622c5e946c2f441b41d924c5b55c
Yemdjih Kaze Nasser <kaze.nasser@outlook.com>
2021-01-07 04:54:59 +0100

    Bounties layout footer component

---
+++ src/main/webapp/app/components/footers/Footerer1.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import { Grid, Link, makeStyles } from '@material-ui/core';
+import { Copyright } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    paddingTop: theme.spacing(12),
+    paddingBottom: theme.spacing(12),
+    [theme.breakpoints.up('md')]: {
+      paddingTop: theme.spacing(30),
+      paddingBottom: theme.spacing(30),
+    }
+  },
+  primaryAction: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+      marginRight: theme.spacing(0),
+      marginBottom: theme.spacing(2),
+    }
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+export default function Footer1(props) {
+  const classes = useStyles();
+  
+  const content = {
+    'badge': 'The New Internet',
+    'header': 'We\'ve built a decentralized internet where information is totally free',
+    'primary-action': 'Read more',
+    'pattern': 'content/images/bg/pattern1.png',
+    ...props.content
+  };
+
+  const footers = [
+    {
+      title: 'Company',
+      description: ['Team', 'History', 'Contact us', 'Locations'],
+    },
+    {
+      title: 'Features',
+      description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
+    },
+    {
+      title: 'Resources',
+      description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    },
+    {
+      title: 'Legal',
+      description: ['Privacy policy', 'Terms of use'],
+    },
+  ];
+
+  return (
+    <Container maxWidth="md" component="footer" className={classes.footer}>
+      <Grid container spacing={4} justify="space-evenly">
+        {footers.map((footer) => (
+          <Grid item xs={6} sm={3} key={footer.title}>
+            <Typography variant="h6" color="textPrimary" gutterBottom>
+              {footer.title}
+            </Typography>
+            <ul>
+              {footer.description.map((item) => (
+                <li key={item}>
+                  <Link href="#" variant="subtitle1" color="textSecondary">
+                    {item}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Grid>
+        ))}
+      </Grid>
+      <Box mt={5}>
+        <Typography variant="subtitle1" color="textPrimary" align='center' gutterBottom>
+          Copyright <Copyright /> Your Website 2021.
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
