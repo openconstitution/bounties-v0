2361d7fb6156b4d770721ebeb49a4582a5dfbf7e
Fogha <collinslord50@gmail.com>
2021-01-29 16:59:43 +0100

    fixing dummy data duplicates

---
+++ src/main/webapp/app/data.ts
@@ -0,0 +1,44 @@
+export const bountyData = [
+  {
+    header: true,
+    name: 'NAME',
+    type: 'TYPE',
+    Difficulty: 'DIFFICULTY',
+    amount: 'AMOUNT',
+  },
+  {
+    header: false,
+    name: 'KGB#110',
+    type: 'FE',
+    Difficulty: 'Expert',
+    amount: '$90',
+  },
+  {
+    header: false,
+    name: 'KGB#111',
+    type: 'FE',
+    Difficulty: 'Intermediate',
+    amount: '$40',
+  },
+  {
+    header: false,
+    name: 'WKM#187',
+    type: 'BE',
+    Difficulty: 'Intermediate',
+    amount: '$200',
+  },
+  {
+    header: false,
+    name: 'WKM#188',
+    type: 'BE',
+    Difficulty: 'Expert',
+    amount: '$250',
+  },
+  {
+    header: false,
+    name: 'WKM#387',
+    type: 'BE',
+    Difficulty: 'Expert',
+    amount: '$300',
+  },
+];
