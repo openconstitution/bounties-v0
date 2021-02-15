4d968d7556a06654cd7612333ae3d808ec451e34
xurror <kaze.nasser@outlook.com>
2021-02-15 11:21:15 +0100

    Make js tests collect packages from root node_modules

--- src/test/javascript/spec/enzyme-setup.ts
+++ src/test/javascript/spec/enzyme-setup.ts
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
-import Adapter from './app/tests/node_modules/enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
 // React 16 Enzyme adapter
 configure({ adapter: new Adapter() });
